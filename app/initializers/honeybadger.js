8dc69b9f6571baaff8dad5ff61debd0c10cfb0f3
Eric Baril <baril27@gmail.com>
2016-03-31 15:53:50 -0700

    Use Ember alias for jQuery

--- app/initializers/honeybadger.js
+++ app/initializers/honeybadger.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export function initialize() {
-  $.getScript('//js.honeybadger.io/v0.2/honeybadger.min.js').then(function() {
+  Ember.$.getScript('//js.honeybadger.io/v0.2/honeybadger.min.js').then(function() {
     if (window.Honeybadger) {
       var apiKey = Ember.get(ENV, 'honeybadger.apiKey');
       var environment = Ember.get(ENV, 'honeybadger.environment');
