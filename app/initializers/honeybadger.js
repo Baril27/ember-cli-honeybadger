d2b045778393bbeb257aab345c56ad1169f695d6
Eric Baril <baril27@gmail.com>
2016-03-31 15:54:51 -0700

    Bump script version and remove gloabl window object

--- app/initializers/honeybadger.js
+++ app/initializers/honeybadger.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export function initialize() {
-  Ember.$.getScript('//js.honeybadger.io/v0.2/honeybadger.min.js').then(function() {
-    if (window.Honeybadger) {
+  Ember.$.getScript('//js.honeybadger.io/v0.4/honeybadger.min.js').then(function() {
+    if (Honeybadger) {
       var apiKey = Ember.get(ENV, 'honeybadger.apiKey');
       var environment = Ember.get(ENV, 'honeybadger.environment');
       var debug = Ember.get(ENV, 'honeybadger.debug') || false;
 
-      window.Honeybadger.configure({
+      Honeybadger.configure({
         api_key: apiKey,
         environment: environment,
         debug: debug,
